d239cb810791537297a347cc722a80158023b54d
IcyFlame1423 <justinchen07@gmail.com>
2024-08-18 23:14:14 -0400

    8/18/24

--- src/main/java/sbmod/SkyblockMod.java
+++ src/main/java/sbmod/SkyblockMod.java
@@ -1,12 +1,10 @@
 package sbmod;
-
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.Instance;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-import sbmod.proxy.CommonProxy;
+import sbmod.listeners.PlayerListener;
+
 
 @Mod(modid = SkyblockMod.MODID, name = SkyblockMod.MOD_NAME, version = SkyblockMod.VERSION, acceptedMinecraftVersions = SkyblockMod.ACCEPTED_MC_VERSION)
 public class SkyblockMod {
@@ -14,30 +12,24 @@ public class SkyblockMod {
     public static final String VERSION = "1.0.0";
     public static final String MOD_NAME = "Icclaw";
     public static final String ACCEPTED_MC_VERSION = "[1.8.9]";
-    public static final String CLIENT_PROXY_CLASS = "sbmod.proxy.ClientProxy";
-    public static final String SERVER_PROXY_CLASS = "sbmod.proxy.CommonProxy";
+    private PlayerListener test;
 
 
-    @Instance(SkyblockMod.MODID)
-    public static SkyblockMod instance;
 
-    @SidedProxy(clientSide = SkyblockMod.CLIENT_PROXY_CLASS, serverSide = SkyblockMod.SERVER_PROXY_CLASS)
-    public static CommonProxy proxy;
 
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
-        proxy.preInit();
-        System.out.println("Icclaw pre-initialized");
-    }
 
+        System.out.println("Icyclaw pre-initialized");
+    }
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-        proxy.init();
-        System.out.println("Icclaw initialized");
-    }
 
+        System.out.println("Icyclaw initialized");
+    }
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event) {
-        System.out.println("Icclaw post-initialized");
+        System.out.println("Icyclaw post-initialized");
     }
+
 }
---
+++ src/main/java/sbmod/listeners/PlayerListener.java
@@ -0,0 +1,15 @@
+package sbmod.listeners;
+import net.minecraftforge.client.event.ClientChatReceivedEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+
+public class PlayerListener {
+    @SubscribeEvent
+    public void onChatReceive(ClientChatReceivedEvent e){
+        String unformatted = e.message.getUnformattedText();
+        String formatted = e.message.getFormattedText();
+        /*Mod can respond upon the player receiving a chat message. This should work as it is done the same way
+        as SBA*/
+
+    }
+}
